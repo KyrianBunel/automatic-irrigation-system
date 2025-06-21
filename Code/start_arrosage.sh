5912acad74fdfeadd75e87a57544d9e70d3ceecf
KyrianBunel <136705314+KyrianBunel@users.noreply.github.com>
2025-06-21 20:46:23 +0200

    Add files via upload

---
+++ Code/start_arrosage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 /home/pi/Documents/ArrosageAUTO/gestion_serie.py &
+python3 /home/pi/Documents/ArrosageAUTO/ArrosageAUTO_SERVER.py &
+python3 /home/pi/Documents/ArrosageAUTO/ArrosageAUTO.py
