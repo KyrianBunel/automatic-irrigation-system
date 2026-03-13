271eff8923181495a6042a2b466d7683e01af684
KyrianBunel <136705314+KyrianBunel@users.noreply.github.com>
2026-03-13 13:57:41 +0100

    Delete Code/start_arrosage.sh

--- Code/start_arrosage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 /home/pi/Documents/ArrosageAUTO/gestion_serie.py &
-python3 /home/pi/Documents/ArrosageAUTO/ArrosageAUTO_SERVER.py &
-python3 /home/pi/Documents/ArrosageAUTO/ArrosageAUTO.py
